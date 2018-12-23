bee6bc7b8b1322f4377c46af775227e00b92f634
gunnerr91 <blissvortex@hotmail.com>
2018-12-23 13:05:42 +0000

    added button and styled

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,33 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, TouchableOpacity } from "react-native";
 
 const Button = () => {
-  return <Text>Click me!</Text>;
+  return (
+    <TouchableOpacity style={styles.button}>
+      <Text style={styles.buttonText}>Click me!</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = {
+  buttonText: {
+    alignSelf: "center",
+    color: "#007aff",
+    fontSize: 16,
+    fontWeight: "600",
+    paddingTop: 10,
+    paddingBottom: 10
+  },
+  button: {
+    flex: 1,
+    alignSelf: "stretch",
+    backgroundColor: "#fff",
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: "#007aff",
+    marginLeft: 5,
+    marginRight: 5
+  }
 };
 
 export default Button;
